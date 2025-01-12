f6f0d3d11a40fdca3f980e3e06e84c2edfbb5311
Harounbacha <harounbacha2005@gmail.com>
2025-01-12 14:16:22 +0100

    Fix Gunicorn configuration and update deployment settings

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 # exit on error
 set -o errexit
 
+# Upgrade pip
+python -m pip install --upgrade pip
+
 # Install python dependencies
 pip install -r requirements.txt
 
--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -7,7 +7,7 @@ backlog = 2048
 
 # Worker processes
 workers = multiprocessing.cpu_count() * 2 + 1
-worker_class = 'uvicorn.workers.UvicornWorker'
+worker_class = 'sync'
 worker_connections = 1000
 timeout = 30
 keepalive = 2
